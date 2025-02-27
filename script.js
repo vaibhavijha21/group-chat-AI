1ac8d223a6735fe0b6010b473c5e09a831d36323
Vaibhavi Jha <vaibhavi0154.becse24@chitkara.edu.in>
2025-02-27 22:13:05 +0530

    port change

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:5000");
+const socket = io("https://group-chat-ai-dgiz.onrender.com");
 
 document.getElementById("sendBtn").addEventListener("click", () => {
     sendMessage();
